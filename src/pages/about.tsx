2f6213ce3993c2f4a8b14adf5f16e3e0217dffa1
kandalgaonkarshubham <kandalgaonkarshubham@gmail.com>
2025-03-06 23:14:12 +0530

    feat: add buttons

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useState } from "react";
 
 import { useFirmStore } from "@/store/firm";
 
@@ -8,22 +8,33 @@ import { useFirmStore } from "@/store/firm";
 export default function About() {
   const firms = useFirmStore((state) => state.firms);
 
-  // useEffect(() => {
-  //   console.log(firms)
-  // }, [firms])
+  const [navigationButton, setNavigationButton] = useState(false);
+  const toggleButton = () => setNavigationButton(!navigationButton);
+
+  const router = useRouter()
 
   return (
-    <div
-      className="flex flex-col gap-10 p-8"
-    >
-      <h1 className="text-emerald-600 font-bold text-3xl">About Page</h1>
-      {
-        firms.map((firm) => (
-          <span key={firm.id}>{firm.id} {firm.name}</span>
-        ))
-      }
-      <Link href={"/"} className="bg-neutral-700 rounded p-2 w-fit">Go to Home</Link>
-      {/* <button onClick={() => router.push("/")} className="bg-neutral-700 rounded p-2 w-fit">Go to Home</button> */}
+    <div className="w-full flex items-start justify-between p-8">
+      <div
+        className="flex flex-col gap-10"
+      >
+        <h1 className="text-emerald-600 font-bold text-3xl">About Page</h1>
+        {
+          firms.map((firm) => (
+            <span key={firm.id}>{firm.id} {firm.name}</span>
+          ))
+        }
+
+        {navigationButton ?
+          <Link href={"/"} className="bg-neutral-700 rounded p-2 w-fit">Go to Home</Link>
+          :
+          <button onClick={() => router.push("/")} className="bg-neutral-700 rounded p-2 w-fit">Go to Home</button>
+        }
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <button className="bg-emerald-800 rounded p-2" onClick={toggleButton}>{navigationButton ? "Using LINK" : "Using Router"}</button>
+      </div>
     </div>
   )
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -9,11 +9,10 @@ export default function Home() {
   const fetchALlFirms = useFirmStore((state) => state.fetchAllFirms);
   const cleanFirms = useFirmStore((state) => state.cleanFirms);
 
-  const router = useRouter()
+  const [navigationButton, setNavigationButton] = useState(false);
+  const toggleButton = () => setNavigationButton(!navigationButton);
 
-  // useEffect(() => {
-  //   console.log(firms)
-  // }, [firms])
+  const router = useRouter()
 
   return (
     <div className="w-full flex items-start justify-between p-8">
@@ -26,11 +25,16 @@ export default function Home() {
             <span key={firm.id}>{firm.id} {firm.name}</span>
           ))
         }
-        <Link href={"/about"} className="bg-neutral-700 rounded p-2 w-fit">Go to About</Link>
-        {/* <button onClick={() => router.push("/about")} className="bg-neutral-700 rounded p-2 w-fit">Go to About</button> */}
+
+        {navigationButton ?
+          <Link href={"/about"} className="bg-neutral-700 rounded p-2 w-fit">Go to About</Link>
+          :
+          <button onClick={() => router.push("/about")} className="bg-neutral-700 rounded p-2 w-fit">Go to About</button>
+        }
       </div>
 
       <div className="flex flex-col gap-4">
+        <button className="bg-emerald-800 rounded p-2" onClick={toggleButton}>{navigationButton ? "Using LINK" : "Using Router"}</button>
         <button className="bg-sky-800 rounded p-2" onClick={fetchALlFirms}>Fetch Firms</button>
         <button className="bg-rose-800 rounded p-2" onClick={cleanFirms}>Clean Firms</button>
       </div>
